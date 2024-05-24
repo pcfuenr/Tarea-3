7ef9abb2e5a41dbb1c91453fd06366594bc52eb2
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-24 17:55:33 -0400

    Eliminado Main Tarea-1 y cambio de nombres

---
+++ Tarea-3/src/main/java/org/Grafica/Main.java
@@ -0,0 +1,7 @@
+package org.Grafica;
+import org.Logica.*;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
--- Tarea-3/src/main/java/org/example/Main2.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import org.Logica.*;
-
-public class Main2 {
-    public static void main(String[] args) {
-        Moneda m = new Moneda100();
-        System.out.println(m.getValor());
-    }
-}
