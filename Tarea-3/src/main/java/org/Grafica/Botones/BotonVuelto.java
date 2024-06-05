ed8a88f193f4942e39189a8a4b1de9d0a2afec21
pcfuenr <panchocifuentes1234@gmail.com>
2024-06-05 19:26:08 -0400

    Añadido boton vuelto y obtener producto

---
+++ Tarea-3/src/main/java/org/Grafica/Botones/BotonVuelto.java
@@ -0,0 +1,14 @@
+package org.Grafica.Botones;
+
+import org.Grafica.PantallaExpendedor;
+
+public class BotonVuelto extends Boton {
+    public BotonVuelto(PantallaExpendedor p){
+        super(p,"Vuelto",398, 461, 79, 39);
+    }
+
+    @Override
+    public void botonPresionado() {
+
+    }
+}
